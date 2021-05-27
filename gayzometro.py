6a475797ee6821fb13da3de4d5e890d35901d205
oyasumiruan <84942427+oyasumiruan@users.noreply.github.com>
2021-05-27 20:04:12 -0300

    Add files via upload

---
+++ gayzometro.py
@@ -0,0 +1,16 @@
+from random import randint
+
+pergunta = input('qual o nome do gay? ')
+
+class Gay():
+	
+	def __init__(self, gay_name):
+		self.gay_name = gay_name
+		
+	def porcentagem(self):
+		self.porcento = randint(0, 100)
+		print('O ' + self.gay_name.title() + ' e ' + 
+		str(self.porcento) + '% gay.')
+
+gay = Gay(pergunta)
+gay.porcentagem()
